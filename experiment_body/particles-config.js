3ed86d68634a6599777b2c7754803512da4a9248
Prabhat Sharma <prabhatsharma040407@gmail.com>
2026-03-28 16:02:21 +0530

    Moving Web like Body

---
+++ experiment_body/particles-config.js
@@ -0,0 +1,49 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 70
+    },
+    "color": {
+      "value": "#00ff62"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.7
+    },
+    "size": {
+      "value": 3
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#00ff88",
+      "opacity": 0.6,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.2
+    }
+  },
+
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      }
+    },
+    "modes": {
+      "grab": {
+        "distance": 200,
+        "line_linked": {
+          "opacity": 1
+        }
+      }
+    }
+  },
+
+  "retina_detect": true
+});
